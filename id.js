1644a04f62363a80cb6a39dc245c982c1795322f
Mark Dittmer <mark.s.dittmer@gmail.com>
2016-08-26 08:01:12 -0400

    JS object id fix: Storage objects misbehave when you define properties on them; keep a list of ids for them instead

--- id.js
+++ id.js
@@ -1,26 +1,48 @@
 // Provide a unique id for every object.
 (function() {
-  var next$UID = (function() {
+  var nextId = (function() {
     // Leave space for "reserved IDs". Useful when values may mean
     // "object id or [some other id]".
     var id = 10000;
     return function() { return id++; };
   })();
 
+  // Storage objects misbehave when we attempt to .defineProperty() on them.
+  // Instead, store an id mapping in a list so that when Storage objects are
+  // encountered their ids are managed appropriately.
+  var storageIds = [];
+  function getStorageId(o) {
+    for ( var i = 0; i < storageIds.length; i++ ) {
+      if ( o === storageIds[i][0] ) return storageIds[i][1];
+    }
+
+    var id = nextId();
+    storageIds.push([o, id]);
+    return id;
+  }
+
   Object.defineProperty(
     Object.prototype,
     '$UID',
     {
       get: function() {
-        if ( ! Object.hasOwnProperty.call(this, '$UID__') ) {
-          Object.defineProperty(
-            this,
-            '$UID__',
-            { value: next$UID(), enumerable: false });
+        // Catch Storage object corner case.
+        if ( window.Storage && window.Storage.prototype &&
+             ( window.Storage.protoype === this ||
+               window.Storage.prototype.isPrototypeOf(this) ) ) {
+          return getStorageId(this);
+        }
+
+        if ( ! this.hasOwnProperty('$UID__') ) {
+          var id = nextId();
+          Object.defineProperty(this, '$UID__', {
+            value: nextId(),
+            enumerable: false,
+          });
         }
         return this.$UID__;
       },
-      enumerable: false
+      enumerable: false,
     }
   );
   Object.prototype.$UID;
