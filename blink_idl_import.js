702fbfdc4edd75ea63ef0dd076cf244c7cf3c51d
Mark Dittmer <mark.s.dittmer@gmail.com>
2016-09-14 12:23:20 -0400

    Add Blink IDL importer scripts

---
+++ blink_idl_import.js
@@ -0,0 +1,73 @@
+/**
+ * @license
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+// NOTE: Only to be invoked from blink_idl_import.sh.
+
+var env = require('process').env;
+var idlFiles = env.IDL_FILES.split('\n').map(function(path) {
+  return env.BLINK_SRC_DIR + '/' + path;
+});
+
+var jsonStableStringify = require('json-stable-stringify');
+var jsonStableStringifyConfig = {
+  space: '  ',
+  cmp: function(a, b) {
+    return a.key < b.key ? -1 : 1;
+  },
+};
+function stringify(data) {
+  return jsonStableStringify(data, jsonStableStringifyConfig);
+}
+
+var fs = require('fs');
+function loadFiles(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    arr[i] = {
+      path: arr[i],
+      contents: fs.readFileSync(arr[i]).toString(),
+    };
+  }
+  return arr;
+}
+
+var data = loadFiles(idlFiles);
+
+var parser = require('webIDLParser');
+
+var errCount = 0;
+var parses = [];
+data.forEach(function(datum) {
+  try {
+    var res = parser.parseString(datum.contents);
+    if (res[0]) {
+      parses = parses.concat(res[1]);
+    } else {
+      errCount++;
+      console.warn('Incomplete parse from', datum.path);
+    }
+  } catch (e) {
+    errCount++;
+    console.warn('Exception thrown parsing', datum.path);
+  }
+});
+
+fs.writeFileSync(env.WEB_APIS_DIR + '/data/idl/blink/all.json',
+                 stringify(parses));
+
+console.log('Wrote', parses.length, 'IDL fragments from', idlFiles.length,
+            'files. Encountered', errCount, 'errors');
---
+++ blink_idl_import.sh
@@ -0,0 +1,23 @@
+#!/bin/zsh
+
+NODE_PATH=${NODE_PATH:-.:./static/js}
+WEB_APIS_DIR=${WEB_APIS_DIR:-$(readlink -f $(dirname "$0"))}
+CHROMIUM_SRC_DIR=${CHROMIUM_SRC_DIR:-${HOME}/src/chromium/src}
+BLINK_SRC_DIR=${BLINK_SRC_DIR:-${CHROMIUM_SRC_DIR}/third_party/WebKit}
+
+if [[ ! -d ${WEB_APIS_DIR} ]]; then
+    >&2 echo "ERROR: WEB_APIS_DIR=${WEB_APIS_DIR} is not a directory"
+fi
+if [[ ! -d ${BLINK_SRC_DIR} ]]; then
+    >&2 echo "ERROR: BLINK_SRC_DIR=${BLINK_SRC_DIR} is not a directory"
+fi
+
+pushd ${BLINK_SRC_DIR}
+IDL_FILES=$(git ls-files '*.idl' | grep -v bindings/ | grep -v testing/)
+popd
+
+export NODE_PATH
+export WEB_APIS_DIR
+export BLINK_SRC_DIR
+export IDL_FILES
+node blink_idl_import.js
