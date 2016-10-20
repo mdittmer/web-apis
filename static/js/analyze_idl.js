b36bb1853a4d89fb3188ee834280445347b27dec
Mark Dittmer <markdittmer@google.com>
2016-10-20 16:00:48 -0400

    Update analyze_idl to use jsdifflib to show diff of JSONified WebIDL parse trees

--- static/js/analyze_idl.js
+++ static/js/analyze_idl.js
@@ -17,6 +17,7 @@
 'use strict';
 
 var stdlib = require('ya-stdlib-js');
+var _ = require('lodash');
 
 function Indexer(opts) {
   this.init(opts);
@@ -54,6 +55,7 @@ IDLCollection.prototype.init = function init(opts) {
   this.data = [];
   this.idx = new Indexer();
   this.idx.addIndex('name');
+  this.idx.addIndex('implementer');
   this.idx.addIndex('url');
 };
 
@@ -76,7 +78,6 @@ IDLCollection.fromJSON = function fromJSON(json, opts) {
     var parses = item.parses;
     for (var j = 0; j < parses.length; j++) {
       var parse = parses[j];
-      if (!parse.name && !parse.implementer) debugger;
       parse.url = url;
       coll.put(parse);
     }
@@ -86,9 +87,10 @@ IDLCollection.fromJSON = function fromJSON(json, opts) {
 };
 
 var uiData = {
-  apis: [],
-  structs: [],
-  primitives: [],
+  sources: [],
+  interfaces: [],
+  left: null,
+  right: null,
 };
 
 function hashCode(str) {
@@ -116,40 +118,114 @@ function optValueToURL(label) {
   return '/data/idl/' + label.replace(/ /g, '/');
 }
 
-function analyze() {
-  var leftValue = e('#left-input').value;
-  var rightValue = e('#right-input').value;
-
-  Promise.all([
-    stdlib.xhr(optValueToURL(leftValue), {responseType: 'json'}),
-    stdlib.xhr(optValueToURL(rightValue), {responseType: 'json'}),
-  ]).then(function(jsons) {
-    var left = IDLCollection.fromJSON(jsons[0]);
-    var right = IDLCollection.fromJSON(jsons[1]);
-
-    // var matched = 0;
-    // var unmatched = 0;
-    // for (var i = 0; i < left.data.length; i++) {
-    //   var leftItem = left.data[i];
-    //   var matches = right.find('name', leftItem.name);
-    //   if (matches.length === 0) {
-    //     unmatched++;
-    //   } else {
-    //     matched++;
-    //   }
-    // }
-    // console.log('From left', 'matched', matched, 'unmatched', unmatched);
-
-    for (var i = 0; i < right.data.length; i++) {
-      console.log('Right name', right.data[i].name);
-    }
-  });
+function getData(direction) {
+  var value = e('#' + direction + '-input').value;
+  return stdlib.xhr(optValueToURL(value), {responseType: 'json'}).then(
+      function(json) {
+        if (json === null) return;
+        uiData[direction] = IDLCollection.fromJSON(json);
+      }
+  );
 }
 
-var dataOpts = [];
+function updateInterfaces() {
+  if (!uiData.left) return;
+
+  var datalist = e('#interfaces');
+  datalist.innerHTML = '';
+  addOpts(datalist, uiData.left.data.map(function(item) {
+    return item.name || item.implementer;
+  }));
+}
+
+function diffPrimitive(a, b, keys, ret) {
+  if (a !== b) {
+    ret.push({ keys: keys.slice(), left: a, right: b});
+  } else {
+    ret.score++;
+  }
+  return ret;
+}
+
+function diff_(a, b, keys, ret) {
+  if (a === null || typeof a !== 'object' ||
+      b === null || typeof b !== 'object') {
+    return diffPrimitive(a, b, keys, ret);
+  }
+  ret.score++;
+  var ownKeys = _.union(Object.getOwnPropertyNames(a),
+                        Object.getOwnPropertyNames(b));
+  for (var i = 0; i < ownKeys.length; i++) {
+    var key = ownKeys[i];
+    keys.push(key);
+    diff_(a[key], b[key], keys, ret);
+    keys.pop();
+  }
+  return ret;
+}
+
+function diff(a, b) {
+  var ret = [];
+  ret.score = 0;
+  return diff_(a, b, [], ret);
+}
+
+function analyze() {
+  var iface = e('#interface-input').value;
+  var lefts = uiData.left.find('name', iface).concat(
+      uiData.left.find('implementer', iface));
+  var rights = uiData.right.find('name', iface).concat(
+      uiData.right.find('implementer', iface));
+
+  if (!(lefts.length && rights.length && iface)) return;
+
+  console.log('Found', lefts.length, 'lefts and', rights.length, 'rights for',
+              iface);
+
+  var diffs = [];
+  for (var i = 0; i < lefts.length; i++) {
+    for (var j = 0; j < rights.length; j++) {
+      var data = {
+        left: lefts[i],
+        right: rights[j],
+        diff: diff(lefts[i], rights[j]),
+      };
+      data.score = data.diff.score;
+      diffs.push(data);
+    }
+  }
+  var best = diffs.sort(function(a, b) { return b.score - a.score; })[0];
+
+  // get the baseText and newText values from the two textboxes, and split them into lines
+  var base = difflib.stringAsLines(stdlib.stringify(best.left));
+  var newtxt = difflib.stringAsLines(stdlib.stringify(best.right));
+
+  // create a SequenceMatcher instance that diffs the two sets of lines
+  var sm = new difflib.SequenceMatcher(base, newtxt);
+
+  // get the opcodes from the SequenceMatcher instance
+  // opcodes is a list of 3-tuples describing what changes should be made to the base text
+  // in order to yield the new text
+  var opcodes = sm.get_opcodes();
+  var diffoutputdiv = e('#output');
+  diffoutputdiv.innerHTML = '';
+  var contextSize = null;
+
+  // build the diff view and add it to the current DOM
+  diffoutputdiv.appendChild(diffview.buildView({
+    baseTextLines: base,
+    newTextLines: newtxt,
+    opcodes: opcodes,
+    // set the display titles for each resource
+    baseTextName: e('#left-input').value + ' : ' + e('#interface-input').value,
+    newTextName: e('#right-input').value + ' : ' + e('#interface-input').value,
+    contextSize: contextSize,
+    viewType: 0
+  }));
+}
 
 // Add <option>s to the given <datalist>.
-function addOpts(datalist) {
+function addOpts(datalist, dataOpts) {
   for ( var i = 0; i < dataOpts.length; i++ ) {
     var opt = document.createElement('option');
     opt.value = dataOpts[i];
@@ -160,19 +236,21 @@ function addOpts(datalist) {
 // Get a list of sources the server has data for, and add them to a
 // <datalist>.
 stdlib.xhr('/list/idl', {responseType: 'json'}).then(function(arr) {
-  dataOpts = arr;
-  addOpts(e('#sources'));
+  uiData.sources = arr;
+  addOpts(e('#sources'), uiData.sources);
   if (!loadFromHash()) {
     setupDefaults();
     updateHash();
   }
-  analyze();
+  Promise.all([getData('left'), getData('right')])
+      .then(updateInterfaces).then(analyze);
 });
 
 function updateHash() {
   window.location.hash = 'l=' +
       encodeURIComponent(hashCode(e('#left-input').value)) +
-      '&r=' + encodeURIComponent(hashCode(e('#right-input').value));
+      '&r=' + encodeURIComponent(hashCode(e('#right-input').value)) +
+      '&i=' + encodeURIComponent(e('#interface-input').value);
 }
 
 function loadFromHash() {
@@ -180,21 +258,23 @@ function loadFromHash() {
   if (!hash) return false;
 
   var values = {};
-  ['l', 'r'].forEach(function(name) {
+  ['l', 'r', 'i'].forEach(function(name) {
     values[name] =
-      decodeURIComponent(hash.match(new RegExp(name + '=([^&]*)'))[1]);
+        decodeURIComponent(hash.match(new RegExp(name + '=([^&]*)'))[1]);
   });
   [{key: 'l', name: 'left'}, {key: 'r', name: 'right'}].forEach(
-    function(o) {
-      var datalist = e('#sources');
-      var hash = parseInt(values[o.key]);
-      var name = dataOpts.filter(function(name) {
-        return hashCode(name) === hash;
-      })[0] || '';
-      var input = e('#' + o.name + '-input');
-      input.value = name;
-    }
-  );
+      function(o) {
+        var datalist = e('#sources');
+        var hash = parseInt(values[o.key]);
+        var name = uiData.sources.filter(function(name) {
+          return hashCode(name) === hash;
+        })[0] || '';
+        var input = e('#' + o.name + '-input');
+        input.value = name;
+      }
+      );
+
+  e('#interface-input').value = values.i;
 
   return true;
 }
@@ -204,9 +284,14 @@ function setupDefaults() {
 
 e('#left-input').addEventListener('input', function() {
   updateHash();
+  getData('left').then(updateInterfaces).then(analyze);
   analyze();
 });
 e('#right-input').addEventListener('input', function() {
+  updateHash();
+  getData('right').then(analyze);
+});
+e('#interface-input').addEventListener('input', function() {
   updateHash();
   analyze();
 });
