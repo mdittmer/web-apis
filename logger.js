329367147aa93ced3b1656cce5075cf2eec239ab
Mark Dittmer <markdittmer@google.com>
2016-11-03 21:08:03 -0400

    Pretty logging

--- logger.js
+++ logger.js
@@ -16,19 +16,59 @@
  */
 'use strict';
 
+const colors = require('colors/safe');
+
+const config = {
+  silly: {color: 'rainbow', prefix: '<:-D'},
+  input: {color: 'grey', prefix: '<<<<'},
+  verbose: {color: 'cyan', prefix: 'VERB'},
+  prompt: {color: 'grey', prefix: '>>>>'},
+  info: {color: 'grey', prefix: 'INFO'},
+  data: {color: 'grey', prefix: 'DATA'},
+  log: {color: 'grey', prefix: 'LOGG'},
+  win: {color: 'green', prefix: 'YESS'},
+  help: {color: 'cyan', prefix: 'HELP'},
+  warn: {color: 'yellow', prefix: 'WARN'},
+  debug: {color: 'blue', prefix: 'DEBG'},
+  error: {color: 'red', prefix: 'ERRR'},
+};
+
+let colorsConfig = {};
+let prefixConfig = {};
+Object.getOwnPropertyNames(config).forEach(key => {
+  colorsConfig[key] = config[key].color;
+  prefixConfig[key] = config[key].prefix;
+});
+
+colors.setTheme(colorsConfig);
+
 module.exports = {
   getLogger: function getLogger(info) {
-    const infoStr = JSON.stringify(info);
-    return {
-      log: function() {
-        console.log(infoStr, ...arguments);
-      },
-      warn: function() {
-        console.warn(infoStr, ...arguments);
-      },
-      error: function() {
-        console.error(infoStr, ...arguments);
-      },
-    };
+    const infoStr = JSON.stringify(info) + '\n ';
+    const keys = Object.getOwnPropertyNames(config);
+    let ret = {};
+
+    keys.forEach(key => {
+      const log = (console[key] || console.log).bind(console);
+      const color = colors[key];
+      ret[key] = function() {
+        try {
+          let args = [colors.info(infoStr), color(prefixConfig[key])];
+          for (var i = 0; i < arguments.length; i++) {
+            if (typeof arguments[i] === 'string')
+            args.push(color(arguments[i]));
+            else if (typeof arguments[i].toString === 'function')
+            args.push(color(arguments[i].toString()));
+            else
+            args.push(color(JSON.stringify(arguments[i])));
+          }
+          log.apply(this, args);
+        } catch (err) {
+          console.error(colors.error(`  LERR ${err.toString()}`));
+        }
+      };
+    });
+
+    return ret;
   },
 };
