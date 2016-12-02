66cb6ea6f2d936ed73c6f979fc279212896b9eea
Mark Dittmer <markdittmer@google.com>
2016-12-02 12:52:25 -0500

    Operational multi-command main/memo. Seven changes: (1) run/memo: Multi-command structure; currently supports scrape-blink-linked and scraped-urls (2) BlinkLinkedRunner: Drop commented-out code (3) idl/memos: Clean up code consistency; log issues with network fetch (4) logger: Store latest logs in .../latest to avoid archiving logs multiple times (5) ArrayMemo: Finally get collectValues() right (6) Memo: Style nit: No else for exhaustive return cases (7) MemoBuilder: Default to passthru memo on start()

--- main/memo.es6.js
+++ main/memo.es6.js
@@ -20,28 +20,59 @@ const process = require('process');
 const env = process.env;
 
 const BlinkLinkedRunner = require('../lib/idl/BlinkLinkedRunner.es6.js');
-const debug = require('../lib/debug.es6.js');
+const URLScrapeRunner = require('../lib/idl/URLScrapeRunner.es6.js');
+// const debug = require('../lib/debug.es6.js');
 
 process.on('unhandledRejection', (reason, promise) => {
   console.error(' !!!! unhandledRejection', reason, promise);
   throw reason;
 });
 
-const argv = require('yargs')
-    .usage('$0 [args]')
+const yargs = require('yargs');
+const argv = yargs
+  .command(
+    'scrape-blink-linked [options]',
+    'Scrape WebIDL from web specs linked to by Blink IDL files',
+         () => yargs
+           .alias('b', 'blink-dir')
+           .default('b', `${env.HOME}/src/chromium/src/third_party/WebKit`)
+           .describe('b', 'Chromium Blink source directory for IDL/URL scraping')
 
-    .alias('b', 'blink-dir')
-    .default('b', `${env.HOME}/src/chromium/src/third_party/WebKit`)
-    .describe('b', 'Chromium Blink source directory for IDL/URL scraping')
+           .alias('sre', 'spec-reg-exp')
+           .default('sre', '(dev[.]w3[.]org|[.]github[.]io|spec[.]whatwg[.]org|css-houdini[.]org|csswg[.]org|khronos[.]org|dvcs.w3.org/hg/speech-api/raw-file/tip/webspeechapi[.]html)')
+           .describe('sre', 'Regular expression for identifying URLs as current web specification documents')
+  )
+  .command(
+    'scrape-urls [urls..]',
+    'Scrape WebIDL from specified URLs',
+    () => yargs
+      .describe('urls', 'URLs to scrape for WebIDL')
+      .demand(1, 'urls', 'At least one URL required')
+      .coerce(
+        'urls',
+        urls => urls.map(
+          url => {
+            url = url.match(/^[a-zA-Z0-9_-]+:\/\//) ? url : `https://${url}`;
+            if (!url.match(/^https?:\/\/[^/]+(\/[^?#, \n]*)?(\?[^#, \n]*)?$/))
+              throw new Error(`Invalid anchorless URL: ${url}`);
+            return url;
+          }
+        )
+      )
+  )
+  .demand('command')
+  .help()
+  .argv;
 
-    .alias('sre', 'spec-reg-exp')
-    .default('sre', '(dev[.]w3[.]org|[.]github[.]io|spec[.]whatwg[.]org|css-houdini[.]org|khronos[.]org|dvcs.w3.org/hg/speech-api/raw-file/tip/webspeechapi[.]html)')
-    .describe('sre', 'Regular expression for identifying URLs as current web specification documents')
+const command = argv._[0];
 
-    .help()
-        .argv;
-
-const blinkLinkedRunner = new BlinkLinkedRunner();
-blinkLinkedRunner.configure(argv);
-
-blinkLinkedRunner.run();
+let runner;
+if (command === 'scrape-blink-linked') {
+  runner = new BlinkLinkedRunner();
+} else if (command === 'scrape-urls') {
+  runner = new URLScrapeRunner();
+} else {
+  throw new Error(`Unknown command: ${command}`);
+}
+runner.configure(argv);
+runner.run();
